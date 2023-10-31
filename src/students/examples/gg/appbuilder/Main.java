256876919cd513dc8aff12ddbf91583db480275b
AndreIvanov79 <dyaka1979@gmail.com>
2023-10-31 21:42:09 +0200

    implements getLog() and refactor

--- src/students/examples/gg/appbuilder/Main.java
+++ src/students/examples/gg/appbuilder/Main.java
@@ -10,9 +10,10 @@ import students.examples.gg.appbuilder.task.impl.RunTask;
 
 public class Main {
 	public static void main(String[] args) {
-		TaskRunner runner = new TaskRunnerBuilder().addTask(new CompileTask(), 
-				new Options().set("value1","-d")
-				.set("value2",".").set("value3","C:\\Users\\USER\\eclipse-workspace\\heartbeat-v0.0.1\\src\\greeting\\*.java"))
+		TaskRunner runner = new TaskRunnerBuilder()
+				.addTask(new CompileTask(), new Options().set("value1","-d")
+						.set("value2",".")
+						.set("value3","C:\\Users\\USER\\eclipse-workspace\\heartbeat-v0.0.1\\src\\greeting\\*.java"))
 				.addTask(new RunTask(),new Options().set("value","greeting.HelloWorld"))
 				.addTask(new PackageTask(), new Options().set("value1","cfe")
 						.set("value2", "myapp.jar").set("value3", "HelloWorld")
--- src/students/examples/gg/appbuilder/runner/TaskRunner.java
+++ src/students/examples/gg/appbuilder/runner/TaskRunner.java
@@ -13,36 +13,39 @@ import students.examples.gg.appbuilder.result.Result;
 import students.examples.gg.appbuilder.task.Task;
 
 public class TaskRunner {
-	
+
 	private Task task;
-	
+
 	private Options options;
-	
-	private Map<Task,Options> runnableTasks = new LinkedHashMap<>();
-	
-	private Map<Map<Task,Options>,Result> taskResults = new HashMap<>();
-	
-	public TaskRunner (Map<Task,Options> runnableTasks) {
+
+	private Result taskResult;
+
+	private Map<Task, Options> runnableTasks = new LinkedHashMap<>();
+
+	private Map<Map<Task, Options>, Result> taskResults = new HashMap<>();
+
+	public TaskRunner(Map<Task, Options> runnableTasks) {
 		this.runnableTasks = runnableTasks;
 	}
-	
+
 	public void start() {
 		for (Entry<Task, Options> entry : runnableTasks.entrySet())
 			try {
-				Result taskResult = entry.getKey().execute(entry.getValue());
-				if (taskResult.getExitCode()!=0) {
-					throw new TaskFailureException("BUILDING Failure at step "+entry.getKey().toString());
-				} else {
-				taskResults.put(runnableTasks, taskResult);
-				System.out.println(entry.getKey().getClass().getSimpleName()+": Result code: "+taskResult.getExitCode()+". BUILDING Success!");
+				taskResult = entry.getKey().execute(entry.getValue());
+				if (taskResult.getExitCode() != 0) {
+					throw new TaskFailureException("BUILDING Failure at step " + entry.getKey().toString());
 				}
+				getLog();
 			} catch (InterruptedException | ExecutionException | IOException | TaskFailureException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} 
+			}
 	}
-	
-	Map<Map<Task,Options>,Result> getLog() {
+
+	private Map<Map<Task, Options>, Result> getLog() {
+		taskResults.put(runnableTasks, taskResult);
+		runnableTasks.keySet().forEach(task -> System.out
+				.println(task.getClass().getSimpleName() + " finished with Result: " + taskResult.getExitCode()));
 		return taskResults;
 	}
 
